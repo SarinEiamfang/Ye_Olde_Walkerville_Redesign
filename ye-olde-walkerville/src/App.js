909cbfe1a3838eb5cfeed3075c98dbba804aa4ff
JayPatel202002 <125040098+JayPatel20200@users.noreply.github.com>
2024-02-17 14:30:28 -0500

    Created the project again and added the hero section

--- ye-olde-walkerville/src/App.js
+++ ye-olde-walkerville/src/App.js
@@ -1,11 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
+import Hero from './components/Hero';
 
 function App() {
   return (
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-       </h1>
+    <Hero />
   );
 }
 
